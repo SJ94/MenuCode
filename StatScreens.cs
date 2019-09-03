ea46e7333da375ecb15cfdcd4b8e424afa10a9d8
SJ94 <SJ94@users.noreply.github.com>
2019-09-03 09:57:20 +0200

    Add files via upload

---
+++ StatScreens.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StatScreens : MonoBehaviour
+{
+
+    public Image Background;
+    public Image StatsBackground;
+    public Image LifeText;
+    public Image BubblesSpawnedText;
+    public Image TimeText;
+    public Image StarEmpty1;
+    public Image StarEmpty2;
+    public Image StarEmpty3;
+    public Image GameOverText;
+    public Image TryAgainText;
+    public Image YouDidItText;
+    public Image Star1;
+    public Image Star2;
+    public Image Star3;
+    public Image RookieText;
+    public Image ProText;
+    public Image MasterText;
+    public Button BackButton;
+    public Button RetryButton;
+    public Button NextButton;
+    public Text LifeInt;
+    public Text BubblesInt;
+    public Text TimeInt;
+
+    /// <summary>
+    /// Shows the stats-screen when level is lost or finished.
+    /// </summary>
+    /// <param name="time">Time.</param>
+    /// <param name="bubbles">Bubbles.</param>
+    /// <param name="life">Life.</param>
+    public void ShowStatScreen(int life, int bubbles, int time)
+    {
+        LifeInt.text = life.ToString();
+        BubblesInt.text = bubbles.ToString();
+        TimeInt.text = time.ToString();
+
+        if (life == 0)
+        {   //condition for game over screen
+            Background.gameObject.SetActive(true);
+            StatsBackground.gameObject.SetActive(true);
+            LifeText.gameObject.SetActive(true);
+            LifeInt.gameObject.SetActive(true);
+            BubblesSpawnedText.gameObject.SetActive(true);
+            BubblesInt.gameObject.SetActive(true);
+            TimeText.gameObject.SetActive(true);
+            TimeInt.gameObject.SetActive(true);
+            StarEmpty1.gameObject.SetActive(true);
+            StarEmpty2.gameObject.SetActive(true);
+            StarEmpty3.gameObject.SetActive(true);
+            GameOverText.gameObject.SetActive(true);
+            TryAgainText.gameObject.SetActive(true);
+            BackButton.gameObject.SetActive(true);
+            RetryButton.gameObject.SetActive(true);
+        }
+        else if (bubbles > 40)
+        {   //condition for rookie screen
+            Background.gameObject.SetActive(true);
+            StatsBackground.gameObject.SetActive(true);
+            YouDidItText.gameObject.SetActive(true);
+            Star1.gameObject.SetActive(true);
+            RookieText.gameObject.SetActive(true);
+            LifeText.gameObject.SetActive(true);
+            LifeInt.gameObject.SetActive(true);
+            BubblesSpawnedText.gameObject.SetActive(true);
+            BubblesInt.gameObject.SetActive(true);
+            TimeText.gameObject.SetActive(true);
+            TimeInt.gameObject.SetActive(true);
+            NextButton.gameObject.SetActive(true);
+        }
+        else if (bubbles > 30)
+        {   //condition for pro screen
+            Background.gameObject.SetActive(true);
+            StatsBackground.gameObject.SetActive(true);
+            YouDidItText.gameObject.SetActive(true);
+            Star1.gameObject.SetActive(true);
+            Star2.gameObject.SetActive(true);
+            ProText.gameObject.SetActive(true);
+            LifeText.gameObject.SetActive(true);
+            LifeInt.gameObject.SetActive(true);
+            BubblesSpawnedText.gameObject.SetActive(true);
+            BubblesInt.gameObject.SetActive(true);
+            TimeText.gameObject.SetActive(true);
+            TimeInt.gameObject.SetActive(true);
+            NextButton.gameObject.SetActive(true);
+        }
+        else if (bubbles < 30)
+        {   //condition for master screen
+            Background.gameObject.SetActive(true);
+            StatsBackground.gameObject.SetActive(true);
+            YouDidItText.gameObject.SetActive(true);
+            Star1.gameObject.SetActive(true);
+            Star2.gameObject.SetActive(true);
+            Star3.gameObject.SetActive(true);
+            MasterText.gameObject.SetActive(true);
+            LifeText.gameObject.SetActive(true);
+            LifeInt.gameObject.SetActive(true);
+            BubblesSpawnedText.gameObject.SetActive(true);
+            BubblesInt.gameObject.SetActive(true);
+            TimeText.gameObject.SetActive(true);
+            TimeInt.gameObject.SetActive(true);
+            NextButton.gameObject.SetActive(true);
+        }
+    }
+    public void DisableScreen()
+    {
+        Background.gameObject.SetActive(false);
+        StatsBackground.gameObject.SetActive(false);
+        GameOverText.gameObject.SetActive(false);
+        YouDidItText.gameObject.SetActive(false);
+        StarEmpty1.gameObject.SetActive(false);
+        StarEmpty2.gameObject.SetActive(false);
+        StarEmpty3.gameObject.SetActive(false);
+        Star1.gameObject.SetActive(false);
+        Star2.gameObject.SetActive(false);
+        Star3.gameObject.SetActive(false);
+        TryAgainText.gameObject.SetActive(false);
+        RookieText.gameObject.SetActive(false);
+        ProText.gameObject.SetActive(false);
+        MasterText.gameObject.SetActive(false);
+        LifeText.gameObject.SetActive(false);
+        LifeInt.gameObject.SetActive(false);
+        BubblesSpawnedText.gameObject.SetActive(false);
+        BubblesInt.gameObject.SetActive(false);
+        TimeText.gameObject.SetActive(false);
+        TimeInt.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
+        RetryButton.gameObject.SetActive(false);
+        NextButton.gameObject.SetActive(false);
+    }
+
+    public void NextButtonClicked()
+    {
+        Debug.Log("Next clicked");
+        // SceneManager.LoadScene("Menu1");
+        SceneLoader.instance.LoadLevel(0);
+    }
+
+    public void RetryButtonClickled()
+    {
+        SceneLoader.instance.LoadLevel(GameManager.instance.currentLevel.LevelIndex);
+    }
+
+    public void BackButtonClicked()
+    {
+        SceneLoader.instance.LoadLevel(0);
+    }
+
+
+
+
+
+}
---
+++ menuScript.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour 
+{
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	//public Image Lock1;
+	public Image Lock1;
+	public Image Lock2;
+	public Image Lock3;
+	public Image Lock4;
+	public Image Lock5;
+	public Image Lock6;
+	public Image Lock7;
+	public Image Lock8;
+	public Button Level1;
+	public Button Level2;
+	public Button Level3;
+	public Button Level4;
+	public Button Level5;
+	public Button Level6;
+	public Button Level7;
+	public Button Level8;
+	bool lvl1finished = false;
+	bool lvl2finished = false;
+	bool lvl3finished = false;
+	bool lvl4finished = false;
+	bool lvl5finished = false;
+	bool lvl6finished = false;
+	bool lvl7finished = false;
+	bool lvl8finished = false;
+
+	
+    public bool paused = false;
+
+    public InputField debugInput;
+
+	public void DebugLevelSelect()
+	{
+		SelectLevel(System.Convert.ToInt32(debugInput.text));
+	}
+
+
+	public void SelectLevel(int _index)
+	{
+		SceneLoader.instance.LoadLevel(_index);
+	}
+	public void PlayGame () 
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void PlayLvl1 () 
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void PlayLvl2 () 
+	{
+		SceneManager.LoadScene (2);
+	}
+
+	public void PlayLvl3 () 
+	{
+		SceneManager.LoadScene (3);
+	}
+
+	public void PlayLvl4 () 
+	{
+		SceneManager.LoadScene (4);
+	}
+
+	public void PlayLvl5 () 
+	{
+		SceneManager.LoadScene (5);
+	}
+	public void PlayLvl6 () 
+	{
+		SceneManager.LoadScene (6);
+	}
+	public void PlayLvl7 () 
+	{
+		SceneManager.LoadScene (7);
+	}
+	public void PlayLvl8 () 
+	{
+		SceneManager.LoadScene (8);
+	}
+
+	public void SetLevelsFinished ()
+	{
+		if (lvl1finished) {
+			Lock1.gameObject.SetActive(false);
+			Level1.gameObject.SetActive(true);
+		}
+		if (lvl2finished) {
+			Lock2.gameObject.SetActive(false);
+			Level2.gameObject.SetActive(true);
+		}
+		if (lvl3finished) {
+			Lock3.gameObject.SetActive(false);
+			Level3.gameObject.SetActive(true);
+		}
+		if (lvl3finished) {
+			Lock3.gameObject.SetActive(false);
+			Level3.gameObject.SetActive(true);
+		}
+		if (lvl4finished) {
+			Lock4.gameObject.SetActive(false);
+			Level4.gameObject.SetActive(true);
+		}
+		if (lvl5finished) {
+			Lock5.gameObject.SetActive(false);
+			Level5.gameObject.SetActive(true);
+		}
+		if (lvl6finished) {
+			Lock6.gameObject.SetActive(false);
+			Level6.gameObject.SetActive(true);
+		}
+		if (lvl7finished) {
+			Lock7.gameObject.SetActive(false);
+			Level7.gameObject.SetActive(true);
+		}
+		if (lvl8finished) {
+			Lock8.gameObject.SetActive(false);
+			Level8.gameObject.SetActive(true);
+		}
+	}
+
+
+		
+}
